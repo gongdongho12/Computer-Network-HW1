77ae0a99acca00ba7d2b269f402292f0ea4c526f
Dongho Gang <gongdongho12@gmail.com>
2021-04-02 14:54:42 +0900

    입력 로직 추가

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ fileName = 'favicon.ico'
 url = f'https://www.facebook.com/{fileName}'
 r = requests.get(url, allow_redirects=True)
 open(fileName, 'wb').write(r.content)
+
+n1 = input()
+n2 = input()
+ 
+print(n1 + n2)
