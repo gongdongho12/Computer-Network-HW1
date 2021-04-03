a0019fe5b664afba1549445edd2fc4e01d7f4ca1
Dongho Gang <gongdongho12@gmail.com>
2021-04-03 16:37:17 +0900

    update https not support

--- main.py
+++ main.py
@@ -23,40 +23,43 @@ def get_raw_request(request):
 
 def get(url):
   o = urlparse(url)
-  port = (80 if o.scheme == 'http' else 443) if (o.port is None) else o.port
-  fileName = url.split('/')[-1]
-  try: 
-    r = requests.get(url, headers=headers, stream=True)
+  if (o.scheme != 'http'):
+    print(f'Only support http, not {o.scheme}')
+  else:
+    port = (80 if o.scheme == 'http' else 443) if (o.port is None) else o.port
+    fileName = url.split('/')[-1]
+    try: 
+      r = requests.get(url, headers=headers, stream=True)
 
-    raw_request = get_raw_request(r.request)
-    print(raw_request)
-
-    if (r.status_code == 200):
-      file_size = int(r.headers.get('Content-Length', None))
-      block_size = 1024
-      max_bars = np.ceil(file_size / block_size)
-      tenth = 1
-      print(f'Total Size {file_size} bytes')
-      with open(fileName, 'wb') as f:
-        for i, chunk in enumerate(r.iter_content(chunk_size=block_size)):
-          f.write(chunk)
-          currunt_block_size = i * block_size
-          currunt_percent = int(np.around(currunt_block_size / file_size * 100))
-          if (currunt_percent > tenth * 10):
-            print('Current Downloading', f'({currunt_block_size}/{file_size}) (bytes) {currunt_percent}%')
-            tenth += 1
-          time.sleep(0.05)
-      print(f'Download Complete: {fileName}, {file_size}/{file_size}')
-    else:
-      print(f'{r.status_code} Not Found')
-  except requests.exceptions.RequestException as ce:
-    # print('ConnectionError', ce)
-    host = get_host(url)
-    print(f'{o.scheme} {host} {host} {port} {fileName}')
-    print(f'{host}: unknown host')
-    print(f'cannot connect to server {host} {port}')
-  finally:
-    print()
+      raw_request = get_raw_request(r.request)
+      print(raw_request)
+      
+      if (r.status_code == 200):
+        file_size = int(r.headers.get('Content-Length', None))
+        block_size = 1024
+        max_bars = np.ceil(file_size / block_size)
+        tenth = 1
+        print(f'Total Size {file_size} bytes')
+        with open(fileName, 'wb') as f:
+          for i, chunk in enumerate(r.iter_content(chunk_size=block_size)):
+            f.write(chunk)
+            currunt_block_size = i * block_size
+            currunt_percent = int(np.around(currunt_block_size / file_size * 100))
+            if (currunt_percent > tenth * 10):
+              print('Current Downloading', f'({currunt_block_size}/{file_size}) (bytes) {currunt_percent}%')
+              tenth += 1
+            time.sleep(0.05)
+        print(f'Download Complete: {fileName}, {file_size}/{file_size}')
+      else:
+        print(f'{r.status_code} Not Found')
+    except requests.exceptions.RequestException as ce:
+      # print('ConnectionError', ce)
+      host = get_host(url)
+      print(f'{o.scheme} {host} {host} {port} {fileName}')
+      print(f'{host}: unknown host')
+      print(f'cannot connect to server {host} {port}')
+    finally:
+      print()
 
 while(True):
   print('> ', end='')
